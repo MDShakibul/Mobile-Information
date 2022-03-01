f614e61ddcdf2d6568bdbe71090d862519317891
MDShakibul <sakib6610@gmail.com.com>
2022-03-02 00:24:03 +0600

    all done

--- app.js
+++ app.js
@@ -10,9 +10,6 @@ document.getElementById("btn-search").addEventListener("click", () => {
 });
 
 const displayPhone = (phones) => {
- 
- 
-
   const resultField = document.getElementById("search-result");
   resultField.textContent = "";
   const singlePhone = document.getElementById('phone-info');
@@ -21,25 +18,34 @@ const displayPhone = (phones) => {
   document.getElementById('text_show_result').innerText = 'All Mobile Phone';
   var limit_Phones = phones.slice(0,20);
 
-  limit_Phones?.forEach((phone) => {
-    
-    const div = document.createElement("div");
-    div.classList.add("col");
-    div.innerHTML = `
-        <div class="card h-100 border border-success border-3 rounded-3">
-            <img src="${phone.image}" class="card-img-top" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">Phone Name: ${phone.phone_name}</h5>
-              <p class="card-text">
-                Brand Name: ${phone.brand}
-              </p>
-              <button onclick="moreDetails('${phone.slug}')" class="btn btn-primary rounded-3">More Info</button>
-            </div>
-          </div>`;
-          
+  if(limit_Phones.length === 0){
+    document.getElementById("error_message").innerText = "This Phone of brand are not available"
+    document.getElementById('text_show_result').innerText="";
+  }else{
+    limit_Phones.forEach((phone) => {
+      document.getElementById("error_message").innerText = ""
+      document.getElementById('error_single_phone_details').innerText = '';
+      
+      const div = document.createElement("div");
+      div.classList.add("col");
+      div.innerHTML = `
+          <div class="card h-100 border border-success border-3 rounded-3">
+              <img src="${phone.image}" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">Phone Name: ${phone.phone_name}</h5>
+                <p class="card-text">
+                  Brand Name: ${phone.brand}
+                </p>
+                <button onclick="moreDetails('${phone.slug}')" class="btn btn-primary rounded-3">More Info</button>
+              </div>
+            </div>`;
+            
+  
+      resultField.appendChild(div);
+    });
+  }
 
-    resultField.appendChild(div);
-  });
+  
 };
 
 
@@ -55,27 +61,33 @@ const displayPhoneDetails = phoneDetails =>{
   singlePhone.textContent = "";
   const div = document.createElement("div");
     div.classList.add("col");
-    div.innerHTML =  `
+    if(!phoneDetails.others){
+      document.getElementById('error_single_phone_details').innerText = 'This Phone have some feature missing';
+      document.getElementById('text_single_phone_details').innerText = '';
+    }else{
+      document.getElementById('error_single_phone_details').innerText="";
+      div.innerHTML =  `
     <div class="card h-100 border border-success border-3 rounded-3">
         <img src="${phoneDetails.image}" class="card-img-top" alt="..." />
         <div class="card-body">
-          <h3 class="card-title">Phone Name: ${phoneDetails.name}</h3>
+          <h3>Phone Name: <span class="fs-5">${phoneDetails.name}</span></h3><hr>
 
           
-          <p5 class="card-title fs-4">Brand Name: <span class="fs-5">${phoneDetails.brand}</span></p5>
-          <h5 class="card-title fs-4">Release Date: <span class="fs-5">${phoneDetails.releaseDate ? phoneDetails.releaseDate : "Release Not Yet"}</span></h5>
-          <h5 class="card-title fs-4">Storage: <span class="fs-5">${phoneDetails.mainFeatures.storage}</span></h5>
-          <h5 class="card-title fs-4">ChipSe: <span class="fs-5">${phoneDetails.mainFeatures.chipSet ? phoneDetails.mainFeatures.chipSet : "Not Available"}</span></h5>
-          <h5 class="card-title fs-4">DisplaySize: <span class="fs-5">${phoneDetails.mainFeatures.displaySize}</span></h5>
+          <h3>Brand Name: <span class="fs-5">${phoneDetails.brand}</span></h3><hr>
+          <h3>Release Date: <span class="fs-5">${phoneDetails.releaseDate ? phoneDetails.releaseDate : "Release Not Yet"}</span></h3><hr>
+          <h3>Storage: <span class="fs-5">${phoneDetails.mainFeatures.storage}</span></h3><hr>
+          <h3>ChipSe: <span class="fs-5">${phoneDetails.mainFeatures.chipSet ? phoneDetails.mainFeatures.chipSet : "Not Available"}</span></h3><hr>
+          <h3>DisplaySize: <span class="fs-5">${phoneDetails.mainFeatures.displaySize}</span></h3><hr>
           
-          <h5 class="card-title fs-4">MainFeatures: <span class="fs-5">${phoneDetails.mainFeatures.memory}</span></h5>
-          <h6 class="card-text">
-            Sensor: ${phoneDetails.mainFeatures.sensors ? phoneDetails.mainFeatures.sensors : "Not Available"}
-          </h6>
-          <h6> Other: <span class="fs-5">${phoneDetails.others.WLAN}</span></h6>
-          <h5 class="card-text">WLAN: ${phoneDetails.others.WLAN}</h5>
+          <h3>MainFeatures: <span class="fs-5">${phoneDetails.mainFeatures.memory}</span></h3><hr>
+          <h3>Sensor: <span class="fs-5">
+             ${phoneDetails.mainFeatures.sensors ? phoneDetails.mainFeatures.sensors : "Not Available"}</span>
+          </h3><hr>
+          <h3> Other: <span class="fs-5">Wlan: ${phoneDetails.others.WLAN ? phoneDetails.others.WLAN : "Not Available"},  Bluetooth: ${phoneDetails.others.Bluetooth}, GPS: ${phoneDetails.others.GPS}, USB: ${phoneDetails.others.USB}</span></h3>
         </div>
       </div>`;
     singlePhone.appendChild(div);
+    }
+    
 
 }
